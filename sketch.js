315c957c7aef2d690bc08babe4c3d297a226da08
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2022-01-07 09:52:20 -0500

    draw snowman - day 1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
 function setup() {
-    createCanvas(400,400);
+  createCanvas(400, 400);
 }
 
 function draw() {
-    background(220);
+  background(220);
+  circle(200,300,150)
+  circle(200,200,100)
+  circle(200,125,50)
+  rect(0,375,400,50)
+  fill("yellow")
+  circle(50,50,50)
 }
